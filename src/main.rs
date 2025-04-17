58acbbf5dfde0efe5f5aace85f14d816bad25a09
Ivan Hackronov <hackr0n777@gmail.com>
2025-04-17 22:50:54 +0300

    promt update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,84 @@
-f = open("9-253.txt")
-count = 0
-k = 0
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
 
-for i in f:
-    s = i.split()
-    a = sorted([int(x) for x in s])
-    a1 = [x for x in a if a.count(x) == 3]
-    a2 = [x for x in a if a.count(x) == 1]
-    a3 = [x for x in a1 if a1.count(x) == 3]
-    if len(a3) == 6 and sum(a3)/6 < a2[0]:
-        k = k +a2[0]
-print(k)
+typedef struct node{
+    int value; 
+    struct node *next;
+}Node;
+
+typedef struct stack{
+    Node *front;
+}Stack;
+
+int init(Stack *q){
+    if(q == NULL){return -1;}
+    q -> front = NULL;
+    return 0;
+}
+
+
+
+
+int push(Stack *q, int val){
+    if(q == NULL) return -1;
+    Node *tmp = malloc(sizeof(Node));
+    if(tmp == NULL){return -2;}
+    tmp -> value = val;
+    tmp -> next = q -> front;
+    q -> front = tmp;
+    return 0;
+}
+
+int pop(Stack *q){
+    if(q == NULL) return -1;
+    if(q -> front == NULL) return -2;
+    Node *tmp = q -> front;
+    if(tmp == NULL) return -3;
+    int val = tmp -> value;
+    q -> front = q -> front -> next;
+    free(tmp);
+    return val;
+}
+
+
+
+int clear(Stack *q){
+    if(q == NULL) return -1;
+    if(q -> front == NULL) return 1;
+    while(q -> front != NULL){
+        Node *tmp = q -> front;
+        q -> front = q -> front -> next;
+        free(tmp);
+    }
+    return 0;
+}
+
+int printStack(Stack *q){
+    if(q == NULL){return -1;}
+    if(q -> front == NULL){printf("aaaa");return -2;}
+    Node *tmp = q -> front;
+    while(tmp != NULL){
+        printf("%d ", tmp -> value);
+        tmp = tmp -> next;
+    }
+    printf("\n");
+    return 0;
+}
+
+
+
+int main() {
+    Stack *q = malloc(sizeof(Stack));
+    init(q);
+    
+    push(q, 10);push(q, 11);push(q, 13);
+
+    printStack(q);
+    pop(q);
+    printStack(q);
+    clear(q);
+    printStack(q);
+    
+    return 0;
+}
