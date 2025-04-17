72080587aeb736d3ee9eae1b4e8ae3a62052fad3
Ivan Hackronov <hackr0n777@gmail.com>
2025-04-17 22:27:26 +0300

    promt update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-money = 9
-
-mas = [1,2,5,10,50,100,500,1000,2000,5000]
-result = [[],[],[],[]]
-
-
-
-def rub(money,mas):
-    cup = [5,2,2,1]
-    for i in range(len(mas) - 1, -1, -1):
-        if money >= mas[i]:
-            money -= mas[i]
-            result.append(mas[i])
-    
-
-
-
-
-rub(money,mas)
-print(result)
+fn main() {
+    println!("Hello World!");
+}
