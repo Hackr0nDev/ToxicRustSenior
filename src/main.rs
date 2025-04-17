dd41509df3e86dc5ce0eb2250dac17a69ecf31d2
Ivan Hackronov <hackr0n777@gmail.com>
2025-04-17 22:22:18 +0300

    promt update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
-fn main() {
-    let total: usize = (0..10_000).sum();
-    println!("Total: {}", total);
-}
+money = 9
+
+mas = [1,2,5,10,50,100,500,1000,2000,5000]
+result = [[],[],[],[]]
+
+
+
+def rub(money,mas):
+    cup = [5,2,2,1]
+    for i in range(len(mas) - 1, -1, -1):
+        if money >= mas[i]:
+            money -= mas[i]
+            result.append(mas[i])
+    
+
+
+
+
+rub(money,mas)
+print(result)
