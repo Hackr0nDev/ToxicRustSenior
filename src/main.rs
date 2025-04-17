8689ed36f3acd73d4789ca538f01daa56215be1b
Ivan Hackronov <hackr0n777@gmail.com>
2025-04-17 22:43:15 +0300

    promt update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello World!"); // как ускорить?
-}
+f = open("9-253.txt")
+count = 0
+k = 0
+for i in f:
+    s = i.split()
+    a = sorted([int(x) for x in s])
+    a1 = [x for x in a if a.count(x) == 3]
+    a2 = [x for x in a if a.count(x) == 1]
+    a3 = [x for x in a1 if a1.count(x) == 3]
+    if len(a3) == 6 and sum(a3)/6 < a2[0]:
+        k = k +a2[0]
+print(k)
