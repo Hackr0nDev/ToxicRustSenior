d9aa7c3b91b4ef0c7463c2eb900fb9b614dd971d
Ivan Hackronov <hackr0n777@gmail.com>
2025-04-17 21:35:51 +0300

    test gpt own file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    let mut data = Vec::new();
-    for i in 0..10_000 {
-        data.push(i.to_string().clone());
-    }
-
-    let total: usize = data
-        .iter()
-        .map(|s| s.parse::<usize>().unwrap())
-        .fold(0, |acc, x| acc + x);
-
+    let data: Vec<usize> = (0..10_000).collect();
+    let total: usize = data.iter().sum();
     println!("Total: {}", total);
 }
